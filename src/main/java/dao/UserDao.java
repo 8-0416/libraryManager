7278a30fb7a3bf3dc198dbf96b543c6c0b27b11f
stand-fast <1553162002@qq.com>
2019-10-30 17:57:19 +0800

    add

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,12 @@
+package dao;
+
+import po.User;
+
+/**
+ * @author FHJ
+ * @date 2019/10/30 16:32
+ */
+public interface UserDao {
+    // 根据userId查询用户
+    User findUserByUserId(String userId);
+}
--- src/main/java/utils/JwtUtil.java
+++ src/main/java/utils/JwtUtil.java
@@ -3,17 +3,10 @@ package utils;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.sun.org.apache.xml.internal.security.algorithms.SignatureAlgorithm;
-import com.sun.org.apache.xml.internal.security.algorithms.SignatureAlgorithmSpi;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-import java.io.UnsupportedEncodingException;
-import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,54 +14,51 @@ import java.util.Map;
 /**
  * @author FHJ
  * @date 2019/10/27 09:12
- * <p>
  * JWT工具类
  */
 public class JwtUtil {
     public static final Logger log = LoggerFactory.getLogger(JwtUtil.class);
 
-    // 设置过期时间为30分钟
+    // 超时时间为30分钟
     private static final long EXPIRE_TIME = 30 * 60 * 1000;
-    // JWT加密密钥
-    private static final String TOKEN_SECRET = "JKV1QiLCJhbGciOiJIUzI1NiJ9";
+
+    // token密钥
+    private static final String TOKEN_SECRET = "r2a35g7s96g4j2k7s3a6t7eb14k27gkq";
 
     /**
-     * 生成JWT字符串
+     * 生成签名，返回jwt字符串
      *
-     * @param userId：学号或者工号
+     * @param userId
      * @return
      */
-    public static String generateJWT(String userId) {
-
-        // 过期时间
-        Date expirationTime = new Date(System.currentTimeMillis() + EXPIRE_TIME);
-
+    public static String sign(String userId) {
         try {
-            // 加密算法
+            // 过期时间
+            Date expiration_time = new Date(System.currentTimeMillis() + EXPIRE_TIME);
+            // 私钥及加密算法
             Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
-            Map<String, Object> headerMap = new HashMap<>(2);
-            headerMap.put("type", "JWT");
-            headerMap.put("alg", "HS256");
-            String jwt = JWT.create().withHeader(headerMap)
-                    .withClaim("userId", userId)
-                    .withExpiresAt(expirationTime)
-                    .sign(algorithm);
-            return jwt;
-        } catch (UnsupportedEncodingException e) {
-            log.error("jwt生成失败！\n" + e.getMessage());
+            // 设置头部信息
+            Map<String, Object> header = new HashMap<>();
+            header.put("type", "JWT");
+            header.put("alg", "HS256");
+            // 附带userId信息，生成签名
+            return JWT.create().withHeader(header).withClaim("userId", userId)
+                    .withExpiresAt(expiration_time).sign(algorithm);
+        } catch (Exception e) {
+            log.error("token生成发生错误");
+            log.error(e.getMessage());
             return null;
         }
     }
 
-
     /**
      * 校验token是否正确
      *
-     * @param token 密钥
-     * @return 是否正确
+     * @param token
+     * @return
      */
-    public static boolean verifyToken(String token) {
-        DecodedJWT decodedJWT = null;
+    public static boolean verify(String token) {
+        DecodedJWT decodedJWT;
         try {
             Algorithm algorithm = Algorithm.HMAC256(TOKEN_SECRET);
             JWTVerifier verifier = JWT.require(algorithm).build();
@@ -77,22 +67,32 @@ public class JwtUtil {
             log.info("载体信息：" + decodedJWT.getClaim("userId").asString());
             log.info("算法：" + decodedJWT.getAlgorithm());
             return true;
-        } catch (Exception exception) {
+        } catch (Exception e) {
+            log.error(e.getMessage());
             return false;
         }
     }
 
-
     /**
-     * 获取登陆用户ID
+     * 获取过期时间
+     *
+     * @param token
+     * @return
      */
-    public static int getUid(String token) {
-        try {
-            DecodedJWT decodedJWT = JWT.decode(token);
-            return decodedJWT.getClaim("userId").asInt();
-        } catch (JWTDecodeException e) {
-            return 0;
-        }
+    public static Date getExpiresAt(String token){
+        DecodedJWT jwt = JWT.decode(token);
+        return jwt.getExpiresAt();
     }
 
+    /**
+     * 获取token中的userId
+     *
+     * @param token
+     * @return
+     */
+    public  static String getUserId(String token){
+        DecodedJWT jwt = JWT.decode(token);
+        return jwt.getClaim("userId").asString();
+    }
 }
+
